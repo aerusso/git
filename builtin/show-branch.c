63f3fb1b0fc72833d80454e6cdebfc6a5638c2e6
Antonio Russo <aerusso@aerusso.net>
2020-12-05 20:47:06 -0700

    Teach git-rev-list --ignore-merge-bases
    This new option simplifies the graph of commits for merges.  Edges to
    commits reachable from more leftward parents, or more leftward parents
    of recursive child commits, are removed.  This "skeleton" spanning tree
    effectively omits all information about merge bases.
    
    When multiple tip commits are specified, all commits reachable from each
    tip commit (but not earlier tips) are simplified as above to form a
    "component".  All edges between components are displayed.
    
    When used with --graph, this amputation can dramatically reduce the
    width of the displayed graph and the total time taken to draw all
    output.
    
    Signed-off-by: Antonio Russo <aerusso@aerusso.net>

--- builtin/show-branch.c
+++ builtin/show-branch.c
@@ -952,7 +952,7 @@ int cmd_show_branch(int ac,
 	}
 
 	/* Sort topologically */
-	sort_in_topological_order(&seen, sort_order);
+	sort_in_topological_order(&seen, NULL, sort_order);
 
 	/* Give names to commits */
 	if (!sha1_name && !no_name)
--- commit.h
+++ commit.h
@@ -202,6 +202,7 @@ struct commit_list *commit_list_reverse(struct commit_list *list);
 void commit_list_free(struct commit_list *list);
 
 struct rev_info; /* in revision.h, it circularly uses enum cmit_fmt */
+struct skel_info;
 
 const char *repo_logmsg_reencode(struct repository *r,
 				 const struct commit *commit,
@@ -238,7 +239,8 @@ enum rev_sort_order {
  *                            chain together.
  *   REV_SORT_BY_COMMIT_DATE: show eligible commits in committer-date order.
  */
-void sort_in_topological_order(struct commit_list **, enum rev_sort_order);
+void sort_in_topological_order(struct commit_list **, struct skel_info *,
+			       enum rev_sort_order);
 
 struct commit_graft {
 	struct object_id oid;
